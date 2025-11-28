00d865f3a12a3d7997f8a2430ee433ba9a00188e
matiannok-star <241478344+matiannok-star@users.noreply.github.com>
2025-11-28 18:51:28 +0300

    Delete components/sections/ConsultationCalendar.tsx

--- components/sections/ConsultationCalendar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This component has been disabled as per user request to resolve loading errors.
-const ConsultationCalendar = () => {
-  return null;
-};
-
-export default ConsultationCalendar;
